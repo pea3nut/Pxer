0d31cccc7ff3c57663ec368d1d1c9f0e9b5949c3
花生PeA <626954412@qq.com>
2016-10-07 17:55:35 +0800

    修正Greasemonkey启动bug

--- src/pxer.user.js
+++ src/pxer.user.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @name          Pxer-beta-6
+// @version       6.0.2
 // @namespace     https://github.com/pea3nut/Pxer
 // @author        花生PeA
 // @description   pixiv.net Tools
@@ -9,15 +10,23 @@
 // @include       http://www.pixiv.net/search.php*
 // ==/UserScript==
 javascript:void((function() {
-    pxerDefinePxerConfig ={
-        "URL_ROOT":"http://127.0.0.1/github/pxer-beta-6/",
-        "TEMPLATE_URL":'src/view/template.php',
-        "DEBUG":true,
-        "CACHE":false,
+    function loadPxer(){
+        window.pxerDefinePxerConfig ={
+            "URL_ROOT":"http://127.0.0.1/github/pxer-beta-6/",
+            "TEMPLATE_URL":'src/view/template.php',
+            "DEBUG":true,
+            "CACHE":false,
+        };
+        document.head.appendChild(
+            document.createElement("script")
+        ).src
+            =window.pxerDefinePxerConfig['URL_ROOT']+"src/script/launcher.js?"
+            +(new Date()).getTime()
+        ;
     };
-    document.head.appendChild(
-        document.createElement("script")
-    ).src
-        =window.pxerDefinePxerConfig['URL_ROOT']+"src/script/launcher.js?"
-        +(new Date()).getTime()
+    if(document.readyState !=='loading'){
+        setTimeout(loadPxer);
+    }else{
+        document.addEventListener('DOMContentLoaded' ,loadPxer);
+    };
 })());
