9dcbf8102c27df5ae42150d81bc00e5046a91980
simon3000 <simon3000@163.com>
2017-04-07 05:44:13 +0800

    nvm
    先这样吧。。

--- src/app/class/PxerSaver.class.js
+++ src/app/class/PxerSaver.class.js
@@ -2,16 +2,18 @@
  * 继承自printer，我要研究一下Pxer的event控制然后继续写
  */
 class PxerSaver extends PxerPrinter {
-  constructor(ppConfig) {
-    //只能下载图片
-    super({
-        "manga_single": ppConfig.manga_single,
-        "manga_multiple": ppConfig.manga_multiple,
-        "illust_single": ppConfig.illust_single,
-        "illust_multiple": ppConfig.illust_multiple,
-        "ugoira_zip": "no",
-        "ugoira_frames": "no",
-    })
-  }
+    constructor(ppConfig) {
+        //只能下载图片
+        super(ppConfig)
+    }
+
+    /**
+     * 启动下载
+     * @method start
+     * @return {undefined}
+     */
+    start() {
+      
+    }
 
 }
