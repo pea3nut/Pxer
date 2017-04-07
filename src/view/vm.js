41b99e2c5a9f60040aef2246b78d8bc586c484b0
simon3000 <simon3000@163.com>
2017-04-07 21:08:05 +0800

    应该修复了format

--- src/view/vm.js
+++ src/view/vm.js
@@ -1,243 +1,243 @@
-afterLoad(function() {
+afterLoad(function(){
     // 寻找插入点
-    let elt = document.createElement('div');
-    elt.innerHTML = PXER_TEMPLATE
-    let insetElt = (
-        document.getElementById('wrapper') ||
+    let elt =document.createElement('div');
+    elt.innerHTML =PXER_TEMPLATE
+    let insetElt=(
+        document.getElementById('wrapper')||
         document.body
     );
-    insetElt.insertBefore(elt, insetElt.firstChild);
+    insetElt.insertBefore(elt,insetElt.firstChild);
 
     // 运行Vue实例
     new Vue({
-        el: '#pxerApp',
-        data: {
-            pxer: new PxerApp(),
-            showAll: false,
-            state: 'loaded', //[loaded|ready|page|works|finish|re-ready|stop|error]
-            stateMap: {
+        el:'#pxerApp',
+        data:{
+            pxer:new PxerApp(),
+            showAll:false,
+            state:'loaded',//[loaded|ready|page|works|finish|re-ready|stop|error]
+            stateMap:{
                 loaded: '初始完毕',
-                ready: '就绪',
-                page: '抓取页码中',
-                works: '抓取作品中',
+                ready:  '就绪',
+                page:   '抓取页码中',
+                works:  '抓取作品中',
                 finish: '完成',
-                're-ready': '再抓取就绪',
-                stop: '用户手动停止',
-                error: '出错',
+                're-ready':'再抓取就绪',
+                stop:   '用户手动停止',
+                error:  '出错',
             },
-            pxerVersion: window['PXER_VERSION'],
-            showPxerFailWindow: false,
-            runTimeTimestamp: 0,
-            runTimeTimer: null,
-            checkedFailWorksList: [],
-            taskInfo: '',
-            tryFailWroksList: [],
-            showTaskOption: false,
-            taskOption: {
-                limit: '',
-                stopId: '',
+            pxerVersion:window['PXER_VERSION'],
+            showPxerFailWindow:false,
+            runTimeTimestamp:0,
+            runTimeTimer:null,
+            checkedFailWorksList:[],
+            taskInfo:'',
+            tryFailWroksList:[],
+            showTaskOption:false,
+            taskOption:{
+                limit:'',
+                stopId:'',
             },
-            showLoadBtn: true,
-            errmsg: '',
-            userLang: 'cn', //localStorage?
-            lang: {
-                nav: {
-                    load: {
-                        cn: 'Load',
+            showLoadBtn:true,
+            errmsg:'',
+            userLang:'cn',//localStorage?
+            lang:{
+                nav:{
+                    load:{
+                        cn:'Load',
                     },
-                    run: {
-                        cn: 'Run',
+                    run:{
+                        cn:'Run',
                     },
-                    stop: {
-                        cn: 'Stop',
+                    stop:{
+                        cn:'Stop',
                     },
-                    warn: {
-                        cn: 'Warn',
+                    warn:{
+                        cn:'Warn',
                     },
                 },
             },
         },
-        computed: {
-            pageType() {
-                var map = {
-                    'member_works': '作品列表页',
-                    'search': '检索页',
-                    'bookmark_works': '收藏列表页',
-                    'unknown': '未知',
+        computed:{
+            pageType(){
+                var map ={
+                    'member_works':'作品列表页',
+                    'search':'检索页',
+                    'bookmark_works':'收藏列表页',
+                    'unknown':'未知',
                 };
                 return map[this.pxer.pageType];
             },
-            isRunning() {
-                var runState = ['page', 'works'];
-                return runState.indexOf(this.state) !== -1;
+            isRunning(){
+                var runState =['page','works'];
+                return runState.indexOf(this.state)!==-1;
             },
-            worksNum() {
-                return this.pxer.taskOption.limit || this.pxer.worksNum;
+            worksNum(){
+                return this.pxer.taskOption.limit ||this.pxer.worksNum;
             },
-            taskCount() {
-                return Math.ceil(this.worksNum / 20) + +this.worksNum;
+            taskCount(){
+                return Math.ceil(this.worksNum/20)+ +this.worksNum;
             },
-            finishCount() {
-                if (this.state === 'page') {
+            finishCount(){
+                if(this.state==='page'){
                     return this.pxer.taskList.filter(pr => pr.completed).length;
-                } else if (this.state === 'works') {
+                }else if(this.state==='works'){
                     return (
                         this.pxer.taskList.filter(pr => pr.completed).length +
-                        ~~(this.worksNum / 20) +
-                        this.pxer.failList.length
+                        ~~(this.worksNum/20)
+                        +this.pxer.failList.length
                     );
-                } else {
+                }else{
                     return -1;
                 };
             },
-            forecastTime() {
-                if (this.isRunning && this.finishCount) {
+            forecastTime(){
+                if(this.isRunning && this.finishCount){
                     return Math.ceil(
-                        (this.runTimeTimestamp / this.finishCount) * this.taskCount -
-                        this.runTimeTimestamp
+                        (this.runTimeTimestamp/this.finishCount)*this.taskCount
+                        -this.runTimeTimestamp
                     );
-                } else {
+                }else{
                     return -1;
                 };
             },
-            printConfigUgoira: {
-                get() {
+            printConfigUgoira:{
+                get(){
                     return this.pxer.ppConfig.ugoira_zip + '-' + this.pxer.ppConfig.ugoira_frames;
                 },
-                set(value) {
-                    var arr = value.split('-');
-                    this.pxer.ppConfig.ugoira_zip = arr[0];
-                    this.pxer.ppConfig.ugoira_frames = arr[1];
+                set(value){
+                    var arr =value.split('-');
+                    this.pxer.ppConfig.ugoira_zip =arr[0];
+                    this.pxer.ppConfig.ugoira_frames =arr[1];
                 }
             },
-            no_tag_any: {
-                get() {
+            no_tag_any:{
+                get(){
                     return this.pxer.pfConfig.no_tag_any.join(' ');
                 },
-                set(value) {
-                    this.pxer.pfConfig.no_tag_any = value.split(' ');
+                set(value){
+                    this.pxer.pfConfig.no_tag_any =value.split(' ');
                 },
             },
-            no_tag_every: {
-                get() {
+            no_tag_every:{
+                get(){
                     return this.pxer.pfConfig.no_tag_every.join(' ');
                 },
-                set(value) {
-                    this.pxer.pfConfig.no_tag_every = value.split(' ');
+                set(value){
+                    this.pxer.pfConfig.no_tag_every =value.split(' ');
                 },
             },
-            has_tag_some: {
-                get() {
+            has_tag_some:{
+                get(){
                     return this.pxer.pfConfig.has_tag_some.join(' ');
                 },
-                set(value) {
-                    this.pxer.pfConfig.has_tag_some = value.split(' ');
+                set(value){
+                    this.pxer.pfConfig.has_tag_some =value.split(' ');
                 },
             },
-            has_tag_every: {
-                get() {
+            has_tag_every:{
+                get(){
                     return this.pxer.pfConfig.has_tag_every.join(' ');
                 },
-                set(value) {
-                    this.pxer.pfConfig.has_tag_every = value.split(' ');
+                set(value){
+                    this.pxer.pfConfig.has_tag_every =value.split(' ');
                 },
             },
-            showFailTaskList() {
+            showFailTaskList(){
                 return this.pxer.failList
-                    .filter((pfi) => {
-                        return this.tryFailWroksList.indexOf(pfi) === -1;
+                    .filter((pfi)=> {
+                        return this.tryFailWroksList.indexOf(pfi)===-1;
                     });
             },
         },
-        watch: {
-            state(newValue, oldValue) {},
-            isRunning(value) {
-                if (value && this.runTimeTimer === null) {
-                    this.runTimeTimer = setInterval(() => this.runTimeTimestamp++, 1000);
-                } else {
+        watch:{
+            state(newValue,oldValue){},
+            isRunning(value){
+                if(value && this.runTimeTimer===null){
+                    this.runTimeTimer =setInterval(()=> this.runTimeTimestamp++,1000);
+                }else{
                     clearInterval(this.runTimeTimer);
-                    this.runTimeTimer = null;
+                    this.runTimeTimer =null;
                 }
             },
         },
-        methods: {
-            load() {
-                if (this.pxer.pageType === 'works_medium') {
+        methods:{
+            load(){
+                if(this.pxer.pageType==='works_medium'){
                     this.pxer.getThis();
-                    this.showLoadBtn = false;
-                    this.pxer.one('finishWorksTask', () => this.showLoadBtn = true);
-                } else {
-                    this.state = 'ready'
+                    this.showLoadBtn =false;
+                    this.pxer.one('finishWorksTask',()=> this.showLoadBtn =true);
+                }else{
+                    this.state ='ready'
                 }
             },
-            run() {
-                if (this.state === 'ready') {
-                    this.state = 'page';
+            run(){
+                if(this.state==='ready'){
+                    this.state ='page';
                     this.pxer.initPageTask();
-                    this.pxer.one('finishPageTask', () => {
-                        this.state = 'works';
+                    this.pxer.one('finishPageTask',()=> {
+                        this.state ='works';
                         this.pxer.switchPage2Works();
                         this.pxer.executeWroksTask();
                     });
-                    this.pxer.one('finishWorksTask', () => {
-                        this.state = 'finish';
+                    this.pxer.one('finishWorksTask',()=> {
+                        this.state ='finish';
                     });
                     this.pxer.executePageTask();
-                } else if (this.state === 're-ready') {
-                    this.state = 'works';
-                    this.pxer.one('finishWorksTask', () => {
-                        this.state = 'finish';
+                }else if(this.state==='re-ready'){
+                    this.state ='works';
+                    this.pxer.one('finishWorksTask',()=> {
+                        this.state ='finish';
                     });
                     this.pxer.executeFailWroks(this.tryFailWroksList);
-                    this.tryFailWroksList = [];
+                    this.tryFailWroksList =[];
                 }
             },
-            stop() {
-                this.state = 'stop';
+            stop(){
+                this.state ='stop';
                 this.pxer.stop();
             },
-            count() {
-                this.taskInfo = this.pxer.getWorksInfo()
+            count(){
+                this.taskInfo =this.pxer.getWorksInfo()
             },
-            useTaskOption() {
-                this.showTaskOption = false;
-                Object.assign(this.pxer.taskOption, this.taskOption);
+            useTaskOption(){
+                this.showTaskOption =false;
+                Object.assign(this.pxer.taskOption,this.taskOption);
             },
-            formatFailType(type) {
+            formatFailType(type){
                 return {
-                    'empty': '获取内容失败',
-                    'timeout': '获取超时',
-                    'r-18': '限制级作品（R-18）',
-                    'r-18g': '怪诞作品（R-18G）',
-                    'mypixiv': '仅好P友可见的作品',
-                    'parse': '解析错误',
-                }[type] || type;
+                    'empty':'获取内容失败',
+                    'timeout':'获取超时',
+                    'r-18':'限制级作品（R-18）',
+                    'r-18g':'怪诞作品（R-18G）',
+                    'mypixiv':'仅好P友可见的作品',
+                    'parse':'解析错误',
+                }[type] ||type;
             },
-            formatFailSolution(type) {
+            formatFailSolution(type){
                 return {
-                    'empty': '点击左侧链接确认内容正确，再试一次~',
-                    'timeout': '增加最大等待时间再试一次~',
-                    'r-18': '开启账号R-18选项',
-                    'r-18g': '开启账号R-18G选项',
-                    'mypixiv': '添加画师好友再尝试',
-                    'parse': '再试一次，若问题依旧，请<a href="https://github.com/pea3nut/Pxer/issues/5" target="_blank">反馈</a>给花生',
-                }[type] || '要不。。。再试一次？';
+                    'empty':'点击左侧链接确认内容正确，再试一次~',
+                    'timeout':'增加最大等待时间再试一次~',
+                    'r-18':'开启账号R-18选项',
+                    'r-18g':'开启账号R-18G选项',
+                    'mypixiv':'添加画师好友再尝试',
+                    'parse':'再试一次，若问题依旧，请<a href="https://github.com/pea3nut/Pxer/issues/5" target="_blank">反馈</a>给花生',
+                }[type] ||'要不。。。再试一次？';
             },
-            tryCheckedPfi() {
+            tryCheckedPfi(){
                 this.tryFailWroksList.push(...this.checkedFailWorksList);
-                this.checkedFailWorksList = [];
-                this.state = 're-ready';
+                this.checkedFailWorksList =[];
+                this.state ='re-ready';
             },
-            formatTime(s) {
+            formatTime(s){
                 return `${~~(s/60)}:${(s%60>=10)?s%60:'0'+s%60}`
             },
         },
-        created() {
-            window['PXER_VM'] = this;
-            this.pxer.on('error', (err) => {
-                this.errmsg = err;
+        created(){
+            window['PXER_VM'] =this;
+            this.pxer.on('error',(err)=> {
+                this.errmsg =err;
             });
-            this.pxer.on('finishWorksTask', function() {
+            this.pxer.on('finishWorksTask',function(){
                 window.blinkTitle();
             });
         },
