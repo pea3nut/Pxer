e3fdc0ca24f8c62ca885916e12e5189a2f76783d
花生PeA <626954412@qq.com>
2017-04-14 15:28:46 +0800

    又忘了更新版本号

--- dist/launcher.js
+++ dist/launcher.js
@@ -6,7 +6,7 @@
 ~function () {
     var libURL = /**/"dist/lib.js"; //*/'';
     var supportURL = /**/"dist/support.js"; //*/'';
-    window['PXER_VERSION'] = /**/"7.0.2"; //*/'';
+    window['PXER_VERSION'] = /**/"7.0.3"; //*/'';
 
     Promise.resolve().then(function () {
         var script = document.createElement('script');
