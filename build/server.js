e95e2f5884c9e2e1b6832384f99b9eabc831e495
_eternal_flame <eternal_flame-AD@protonmail.com>
2018-07-11 14:59:43 +0800

    simple path traversal prevention

--- build/server.js
+++ build/server.js
@@ -8,6 +8,12 @@ http.createServer(function (request, response) {
 
     var filePath = __dirname + '/..' + request.url.replace(/([^\?]+?)\?.+$/,"$1");
 
+    if (!path.normalize(filePath).startsWith(path.normalize(__dirname+"/../"))) {
+        response.writeHead(403);
+        response.end("denied.");
+        return;
+    }
+
     var extname = path.extname(filePath);
     var contentType = 'text/html';
     
@@ -23,10 +29,8 @@ http.createServer(function (request, response) {
     fs.readFile(filePath, function(error, content) {
         if (error) {
             if(error.code == 'ENOENT'){
-                fs.readFile('./404.html', function(error, content) {
-                    response.writeHead(404, { 'Content-Type': 'text/html' });
-                    response.end('File not found.', 'utf-8');
-                });
+                response.writeHead(404);
+                response.end('File not found.', 'utf-8');
             }
             else {
                 response.writeHead(500);
@@ -46,4 +50,4 @@ http.createServer(function (request, response) {
 }).listen(8125, '127.0.0.1');
 console.log('\x1b[31mWARN:\x1b[0m This server is only designed for your ease of local development & testing and is NOT SAFE FOR PRODUCTION USE!!');
 console.log('Dev Server running at http://127.0.0.1:8125/');
-console.log('Please set your PXER_URL in tampermonkey to http://127.0.0.1:8125/')
+console.log('Please set your PXER_URL in tampermonkey to http://127.0.0.1:8125/');
