5df991b53718c8f940375b0bad0fe5276bafe9f8
_eternal_flame <eternal_flame-AD@protonmail.com>
2018-07-14 00:41:25 +0800

    switch to express

--- build/server.js
+++ build/server.js
@@ -1,53 +1,78 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const program = require('commander');
 
-http.createServer(function (request, response) {
-    
-    console.log('request:' + request.url);
+program
+    .option('-m, --mode <mode>', "server mode")
+    .option('-a, --addr <addr>', "bind address")
+    .option('-p, --port <port>', "bind port")
 
-    var filePath = __dirname + '/..' + request.url.replace(/([^\?]+?)\?.+$/,"$1");
+program.parse(process.argv);
 
-    if (!path.normalize(filePath).startsWith(path.normalize(__dirname+"/../"))) {
-        response.writeHead(403);
-        response.end("denied.");
-        return;
+if (['dev','release', undefined].indexOf(program.mode)===-1) {
+    throw new Error("Invalid mode:"+ program.mode);
+}
+const MODE = program.mode || "dev";
+const ADDR = program.addr || "127.0.0.1";
+const PORT = parseInt(program.port) || 8125;
+
+console.log("Running server in " +MODE)
+
+var app = express();
+
+app.get('/', (req, res) => res.status(303).send("http://pxer.pea3nut.org/"));
+
+addFile =function(fpath, uri) {
+    fpath = path.normalize(fpath);
+    var contentType = "text/plain";
+    switch (path.extname(fpath)) {
+        case ".js"  : contentType = "text/javascript"; break;
+        case ".html": contentType = "text/html"; break;
+        case ".css" : contentType = "text/css"; break;
     }
+    var options = {
+        dotfiles: 'deny',
+        cacheControl: true,
+        headers : {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type"               : contentType,
+        },
+    };
+    switch (MODE) {
+        case "dev"    : options.headers['Cache-Control'] = "no-store, must-revalidate"; break;
+        case "release": options.headers['Cache-Control'] = "max-age=1200"; break;
+    };
+    app.get(uri, (req, res)=>res.sendFile(fpath, options));
+}
 
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-    }
-
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT'){
-                response.writeHead(404);
-                response.end('File not found.', 'utf-8');
-            }
-            else {
-                response.writeHead(500);
-                response.end('Error fetching file: ' + error.message + '\n');
-            }
+addFolder =function(fpath, uri) {
+    fs.readdir(fpath, function(err, filelist) {
+        if (err) {
+            console.log("Error while preloading folder:" +err.message);
+        } else {
+            filelist.forEach(function(fn) {
+                var filepath = path.join(fpath, fn);
+                fs.stat(filepath, function(err, info) {
+                    if (err) {
+                        console.log("Error while reading" + filepath + err.message);
+                    } else {
+                        switch (true) {
+                            case info.isDirectory(): addFolder(filepath, uri + fn + "/"); break;
+                            case info.isFile():      addFile(filepath, uri + fn); break;
+                        }
+                    }
+                })
+            })
         }
-        else {
-            response.writeHead(200, {
-                'Content-Type'               : contentType,
-                'Access-Control-Allow-Origin': "*",
-                'Cache-Control'              : 'no-store, must-revalidate'
-            });
-            response.end(content, 'utf-8');
-        }
-    });
+    })
+}
+addFolder(__dirname +"/../src/", "/src/");
+addFolder(__dirname +"/../dist/", "/dist/");
+addFile(__dirname +"/../jsonp.js", "/jsonp.js");
+addFile(__dirname +"../pxer-dev.user.js", "/pxer-dev.user.js");
+addFile(__dirname +"../pxer-master.user.js", "/pxer-master.user.js");
 
-}).listen(8125, '127.0.0.1');
-console.log('\x1b[31mWARN:\x1b[0m This server is only designed for your ease of local development & testing and is NOT SAFE FOR PRODUCTION USE!!');
-console.log('Dev Server running at http://127.0.0.1:8125/');
-console.log('Please set your PXER_URL in tampermonkey to http://127.0.0.1:8125/');
+app.listen(PORT, ADDR, function () {
+    console.log('Server running at ' + ADDR +":"+ PORT);
+})
