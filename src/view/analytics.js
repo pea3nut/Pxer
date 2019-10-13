7d1c9aed06ea990a13784366cfd392f4792f614b
花生PeA <pea3nut@qq.com>
2019-10-13 20:51:49 +0800

    anonymize user data

--- src/view/analytics.js
+++ src/view/analytics.js
@@ -1,6 +1,12 @@
 (function () {
+    let uid = localStorage.getItem('pxer_uid');
+    if (!uid) {
+        uid = Number(Math.random().toString().split('.')[1]).toString(36);
+        localStorage.setItem('pxer_uid', uid);
+    }
+
     const eventSender = new EventSender('https://point.pea3nut.org/events', {
-        uid: dataLayer[0].user_id,
+        uid,
         app_name: 'pxer-app',
         get event_page() { return pxer.util.getPageType(); },
         get referer() { return location.href; },
