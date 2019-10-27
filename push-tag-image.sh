6e7bb2170a82d13a91af882440539a2a07e08e7c
花生PeA <pea3nut@qq.com>
2019-10-27 14:25:02 +0800

    build image tagged branch name

--- push-tag-image.sh
+++ push-tag-image.sh
@@ -1,5 +1,5 @@
 if [ -n "$TRAVIS_TAG" ] ;then
-    echo "push a image for tag $TRAVIS_TAG"
-    docker tag "pea3nut/pxer:${TRAVIS_COMMIT}" "pea3nut/pxer:${TRAVIS_TAG}"
+    echo "push a image for tag pea3nut/pxer:$TRAVIS_TAG"
+    docker tag "pea3nut/pxer:${TRAVIS_BRANCH}" "pea3nut/pxer:${TRAVIS_TAG}"
     docker push "pea3nut/pxer:${TRAVIS_TAG}"
 fi
