68472fec147596a12ac49b3efd24ef1b0d3eb545
花生PeA <pea3nut@qq.com>
2020-01-17 20:35:09 +0800

    add the support for japanese

--- src/view/i18n.js
+++ src/view/i18n.js
@@ -1,5 +1,5 @@
 pxer.t = function (key) {
-    const defaultLang = 'zh'; // FIXME: change to en when i18n got ready
+    const defaultLang = 'en';
 
     return (
         pxer.util.get(pxer.i18nMap[pxer.lang], key)
