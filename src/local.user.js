8e9644097643322fb09fed01b36f2704ef9ac454
pea3nut <pea3nut@qq.com>
2022-08-28 16:00:47 +0800

    fix: add headars for CORS

--- src/local.user.js
+++ src/local.user.js
@@ -1,20 +1,32 @@
 // ==UserScript==
-// @name          Pxer-local
-// @grant         unsafeWindow
+// @name           Pxer-local
+// @description    Maybe the best tool for pixiv.net for capture pictures
+// @description:zh-CN 可能是目前最好用的P站批量抓图工具
+// @description:en-US Maybe the best tool for pixiv.net for capture pictures
+// @description:ja-JP Pixiv の全てのツールで一番使いやすいバッチキャプチャーソフトかもしれない
+// @icon           https://pxer-app.pea3nut.org/public/favicon.ico
+// @version        7
+// @homepageURL    http://pxer.pea3nut.org/
+// @supportURL     https://github.com/FoXZilla/Pxer/issues/new/choose
+// @author         pea3nut / eternal-flame-AD
+// @grant          unsafeWindow
 // @noframes
-// @include       https://www.pixiv.net*
-// @include       http://www.pixiv.net*
-// @include       http://pxer.pea3nut.org*
-// @include       https://127.0.0.1*
-// @include       http://127.0.0.1*
+// @require        https://pxer-app.pea3nut.org/vue.js
+// @include        https://www.pixiv.net*
+// @include        http://www.pixiv.net*
+// @include        http://pxer.pea3nut.org*
+// @include        https://pxer.pea3nut.org*
 // ==/UserScript==
-void (function () {
+javascript: void(function() {
+
     unsafeWindow['PXER_URL'] = 'https://127.0.0.1:8125/';
     unsafeWindow['PXER_MODE'] = 'local';
+    unsafeWindow['Vue'] = Vue;
 
     // add enter point script in page
     document.documentElement.appendChild(
         document.createElement('script')
     ).src = unsafeWindow['PXER_URL'] + 'src/launcher.js?' + (+new Date);
+
 
 }());
